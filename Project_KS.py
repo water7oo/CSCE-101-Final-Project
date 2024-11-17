3ac2323bbed979be9a9eea299a35ca6e0a50d365
water7oo <watersevens37@gmail.com>
2024-11-16 21:06:46 -0500

    Added Psuedo Code and Algorithms

--- Project_KS.py
+++ Project_KS.py
@@ -4,6 +4,31 @@
 # kodys@email.sc.edu
 # FINAL PROJECT: AIM LABS
 
+#Pseudo Code
 
-#
+# Aim training game, click the turtle with your mouse,
+# turtle moves to a random spot within the screens range, track the time
+# it takes the person to click each turtle
+# A game timer will tick down (30 secs? 60 secs?)
+# Final score will be calculated (Average time per click, total turtles killed)
+
+
+# Average time per click: timer will run, when player clicks turtle, timer will reset to 0 (log and append all times in a list, then find average time taken)
+# Each turtle is 100 points, (maybe inforporate a golden turtle??? = 300 points?)
+# Could have a situation where more than 1 turtle spawns????
+
+## Tasks ##
+
+# Make turtle spawn in random spot
+# Allow player to click turtle, then turtle goes to a random spot on the screen
+# Have timer go up from 0 (time.delta time???, Time resets when turtle is clicked, have boolean triggers when turtle is clicked)
+# compile the last time documented before reset happens, then throw it into a list (append it)
+# Have score counter
+# When game is finished tally points and give a final score
+# give the player the option to play again, this resets the game timer to 60 or 30 and runs the while loop again
+
+
+
+
+import turtle
 
